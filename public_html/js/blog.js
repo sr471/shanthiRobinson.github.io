123d192df3f7af2cbd5bf5d7d4d02f261eb4f5f1
sr471 <shanthi.robinson100@gmail.com>
2016-03-22 16:45:57 -0700

    MiniBlog B9

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function (){
+    var APPLICATION_ID = "11BCF95A-6897-A036-FFE2-C4294610F300",
+        SECRET_KEY ="FAAD96D2-4BE2-1DEB-FFBE-A1132489EF00",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var user = new Backendless.user();
+    user.email = "shanthi.robinson100@gmail.com";
+    user.password = "password";
+    Backendless.userservice.register(user);
+    
+});
