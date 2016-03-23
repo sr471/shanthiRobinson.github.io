533750095bad3e2cdd5a6314662b9f55e0a16ead
sr471 <shanthi.robinson100@gmail.com>
2016-03-22 21:56:39 -0700

    MiniBlog B12

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -6,7 +6,7 @@ $(function (){
     Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
     
    var  dataStore = Backendless.Persistence.of(Posts);
-   var post = new Posts({title: "My First Blog Post", content:"My first blog post content", email:"email@gmail.com"});
+   var post = new Posts({title: "My First Blog Post", content:"My first blog post content", authorEmail:"email@gmail.com"});
    dataStore.save(post);
 });
 
@@ -14,5 +14,5 @@ function Posts(args){
     args = args || {};
     this.title = args.title || "";
     this.content = args.content || "";
-    this.authorEmail = args.emailEmail || "";
+    this.authorEmail = args.authorEmail || "";
 }
