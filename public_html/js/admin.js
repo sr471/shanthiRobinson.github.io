2916875054348cc6a9fe7228ba0bfb223326c67e
sr471 <shanthi.robinson100@gmail.com>
2016-03-24 08:09:24 -0700

    MiniBlog B19

---
+++ public_html/js/admin.js
@@ -0,0 +1,21 @@
+$(function (){
+    var APPLICATION_ID = "11BCF95A-6897-A036-FFE2-C4294610F300",
+        SECRET_KEY ="FAAD96D2-4BE2-1DEB-FFBE-A1132489EF00",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+      
+   var loginScript = $("#login-template").html();
+   var loginTemplate = Handlebars.compile(loginScript);
+  
+   
+   $('.main-container').html(loginTemplate);
+});
+
+function Posts(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
